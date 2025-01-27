8c3a553012ae036e4b0f405de9aa31af4f0f7503
Eric Color <ecolor1123@gmail.com>
2025-01-27 16:25:36 -0600

    .

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductFormComponent } from './product-form/product-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'products',
+        component: ProductListComponent
+    },
+    {
+        path: 'products/:id',
+        component: ProductDetailComponent
+    },
+    {
+        path: 'products/:id/nuevo',
+        component: ProductFormComponent
+    }
+];
